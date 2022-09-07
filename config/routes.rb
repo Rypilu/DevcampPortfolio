38b3c7d527870d782518e3888d3f3d56bb75e7c1
rypilu <rpiluski@gmail.com>
2022-09-07 05:08:43 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  # custom route method singular instead of out of the box plural
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
