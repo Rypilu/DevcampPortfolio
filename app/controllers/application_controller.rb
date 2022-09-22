8cb6c8f4ecccb46f154db84099f5002142f87a12
rypilu <rpiluski@gmail.com>
2022-09-22 08:16:27 -0400

    Added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
-
-  # FYI :configure_permitted_parameters is just a random method name, could be anything.
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-
+  # Abstract controller managing all of the controller functionality for the app.
+  include DeviseAllowList
 end
