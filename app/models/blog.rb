6fdc35e87acf1a064a97e82562d794e165ca9a85
rypilu <rpiluski@gmail.com>
2022-10-05 07:30:31 -0400

    Refactored before action for portfolio setting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,5 +5,14 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
 
-  belongs_to :topic, optional: true
+  belongs_to :topic, optional: true 
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
 end
